211afc6cdcbb438e31ef42bab9d658d044800e0a
Don Garrett <dgarrett@google.com>
2013-04-15 19:02:23 -0700

    Update generate_au_zip.py to use lddtree.py.
    generate_au_zip.py needs to bundle up some executables, along with the
    libraries they depend on. Instead of using one-off code for this, switch
    to using the standard helper script lddtree.py for this purpose.
    
    BUG=chromium:224171
    TEST=Manual testing, including unzipping a file and running binaries by hand.
    
    Change-Id: I5120bf2ebeb25361342e03d5065452bea856f42f
    Reviewed-on: https://gerrit.chromium.org/gerrit/48190
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>

--- build_library/generate_au_zip.py
+++ build_library/generate_au_zip.py
@@ -4,334 +4,150 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""
-  Script to generate a zip file of delta-generator and its dependencies.
-"""
+"""Script to generate a zip file of delta-generator and its dependencies."""
+
 import logging.handlers
 import optparse
 import os
-import re
 import shutil
 import subprocess
-import sys
 import tempfile
 
 # GLOBALS
 image_sign_dir = '~/trunk/src/platform/vboot_reference/scripts/image_signing'
-STATIC_FILES = ['/usr/bin/old_bins/cgpt',
-                '~/trunk/src/scripts/common.sh',
-                '/usr/bin/cros_generate_update_payload',
-                '~/trunk/src/scripts/chromeos-common.sh',
-                os.path.join(image_sign_dir, 'convert_recovery_to_ssd.sh'),
-                os.path.join(image_sign_dir, 'common_minimal.sh'),
-                ]
-
-DYNAMIC_EXECUTABLES = ['/usr/bin/delta_generator',
-                       '/usr/bin/bsdiff',
-                       '/usr/bin/bspatch']
-
-# These files will be ignored when present in the dependancy list.
-BLACK_LIST = [
-    # This library does not exist on disk, but is inserted into the
-    # executable's memory space when the executable is loaded by the kernel.
-    'linux-vdso.so',
+EXECUTABLE_FILES = [
+    '/usr/bin/old_bins/cgpt',
+    '~/trunk/src/scripts/common.sh',
+    '/usr/bin/cros_generate_update_payload',
+    '~/trunk/src/scripts/chromeos-common.sh',
+    os.path.join(image_sign_dir, 'convert_recovery_to_ssd.sh'),
+    os.path.join(image_sign_dir, 'common_minimal.sh'),
+    '/usr/bin/delta_generator',
+    '/usr/bin/bsdiff',
+    '/usr/bin/bspatch',
     ]
-
-# These files MUST be present in the dependancy list.
-WHITE_LIST = [
-    # Update WrapExecutableFiles if this file changes names
-    'ld-linux-x86-64.so.2',
-    ]
-
-LIB_DIR = 'lib.so'
-
 # We need directories to be copied recursively to a dest within tempdir
-RECURSE_DIRS = {'~/trunk/src/scripts/lib/shflags': 'lib/shflags'}
-
-logging_format = '%(asctime)s - %(filename)s - %(levelname)-8s: %(message)s'
-date_format = '%Y/%m/%d %H:%M:%S'
-logging.basicConfig(level=logging.INFO, format=logging_format,
-                    datefmt=date_format)
-
-def CreateTempDir():
-  """Creates a tempdir and returns the name of the tempdir."""
-  temp_dir = tempfile.mkdtemp(suffix='au', prefix='tmp')
-  logging.debug('Using tempdir = %s', temp_dir)
-  return temp_dir
+SHELL_LIBRARIES = {'~/trunk/src/scripts/lib/shflags': 'lib/shflags'}
 
 
-def _SplitAndStrip(data):
-  """Prunes the ldd output, and return a list of needed library names
-    Example of data:
-        linux-vdso.so.1 =>  (0x00007ffffc96a000)
-        libbz2.so.1 => /lib/libbz2.so.1 (0x00007f3ff8782000)
-        libc.so.6 => /lib/libc.so.6 (0x00007f3ff83ff000)
-        /lib64/ld-linux-x86-64.so.2 (0x00007f3ff89b3000)
+def CopyExecutableFiles(elf_binaries, zip_base):
+  """Copy the listed binaries over to the target directory.
+
+  For each binary 'foo' in elf_binaries create:
+
+    zip_base/foo
+      Wrapper script to handle dynamic linking when run:
+
+    zip_base/foo.elf
+      Originarl binary.
+
+    zip_base/lib/...
+      All libraries that foo depends on.
+
+  Args:
+    elf_binaries: List of binaries to copy, along with libraries.
+    zip_base: Directory into which binaries/libraries are to be placed.
+  """
+  elf_binaries = [os.path.expanduser(p) for p in elf_binaries]
+
+  cmd = ['/mnt/host/source/chromite/bin/lddtree',
+         '--copy-to-tree', zip_base,
+         '--copy-non-elfs',
+         '--bindir', '/',
+         '--libdir', '/lib',
+         '--generate-wrappers']
+  cmd += elf_binaries
+
+  subprocess.check_call(cmd)
+
+
+def CopyShellLibraries(shell_libraries, zip_base):
+  """Copy shell library directories into the zip_base.
+
     Args:
-      data: list of libraries from ldd output
-    Returns:
-      list of libararies that we should copy
-
-  """
-  return_list = []
-  for line in data.split('\n'):
-    line = re.sub('.*not a dynamic executable.*', '', line)
-    line = re.sub('.* =>\s+', '', line)
-    line = re.sub('\(0x.*\)\s?', '', line)
-    line = line.strip()
-    if not len(line):
-      continue
-    logging.debug('MATCHED line = %s', line)
-    return_list.append(line)
-
-  return return_list
-
-
-def DepsToCopy(ldd_files):
-  """Returns a list of deps for a given dynamic executables list.
-    Args:
-      ldd_files: List of dynamic files that needs to have the deps evaluated
-      black_list: List of files that we should ignore
-   Returns:
-     List of files that are dependencies
-  """
-  libs = set()
-  for file_name in ldd_files:
-    logging.debug('Running ldd on %s', file_name)
-    cmd = ['/usr/bin/ldd', file_name]
-    stdout_data = ''
-    stderr_data = ''
-
-    try:
-      proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
-      (stdout_data, stderr_data) = proc.communicate(input=None)
-    except subprocess.CalledProcessError, e:
-      logging.error('Command %s failed', cmd)
-      logging.error('error code %s', e.returncode)
-      logging.error('ouput %s', e.output)
-      raise
-
-    if not stdout_data: continue
-
-    logging.debug('ldd for %s = stdout = %s stderr =%s', file_name,
-                  stdout_data, stderr_data)
-
-    libs |= set(_SplitAndStrip(stdout_data))
-
-  result = _ExcludeBlacklist(list(libs), BLACK_LIST)
-  _EnforceWhiteList(list(libs), WHITE_LIST)
-  return result
-
-
-def CopyRequiredFiles(dest_files_root):
-  """Generates a list of files that are required for au-generator zip file
-    Args:
-      dest_files_root: location of the directory where we should copy the files
-  """
-  if not dest_files_root:
-    logging.error('Invalid option passed for dest_files_root')
-    sys.exit(1)
-
-  all_files = DYNAMIC_EXECUTABLES + STATIC_FILES
-  all_files = map(os.path.expanduser, all_files)
-
-  for file_name in all_files:
-    if not os.path.isfile(file_name):
-      logging.error('file = %s does not exist', file_name)
-      sys.exit(1)
-
-  logging.debug('Given files that need to be copied = %s' % '' .join(all_files))
-  all_files
-  for file_name in all_files:
-    logging.debug('Copying file  %s to %s', file_name, dest_files_root)
-    shutil.copy2(file_name, dest_files_root)
-
-  libraries = DepsToCopy(ldd_files=DYNAMIC_EXECUTABLES)
-  lib_dir = os.path.join(dest_files_root, LIB_DIR)
-  os.mkdir(lib_dir)
-  for file_name in libraries:
-    logging.debug('Copying file  %s to %s', file_name, lib_dir)
-    shutil.copy2(file_name, lib_dir)
-
-  for source_dir, target_dir in RECURSE_DIRS.iteritems():
-    logging.debug('Processing directory %s', source_dir)
-    full_path = os.path.expanduser(source_dir)
-    if not os.path.isdir(full_path):
-      logging.error("Directory given for %s expanded to %s doens't exist.",
-                    source_dir, full_path)
-      sys.exit(1)
-  dest = os.path.join(dest_files_root, target_dir)
-  logging.debug('Copying directory %s to %s.', full_path, target_dir)
-  shutil.copytree(full_path, dest)
-
-
-def WrapExecutableFiles(dest_files_root):
-  """Our dynamically linked executalbes have to be invoked use the library
-     versions they were linked with inside the chroot (from libc on), as well
-     as the dynamic linker they were built with inside the chroot.
-
-     So, this code moves the execs to backup names, and then creates a shell
-     script wrapper which invokes them in the proper way.
+      shell_libraries: A dictionary mapping directory_to_copy ->
+                                            directory_relative_zip_base.
+      zip_base: Target directory to copy into.
   """
 
-  for src_exec in DYNAMIC_EXECUTABLES:
-    base_exec = os.path.basename(src_exec)
-    local_exec = os.path.join(dest_files_root, base_exec)
-    local_exec_wrapped = local_exec + ".bin"
-    shutil.move(local_exec, local_exec_wrapped)
-
-    fd = os.open(local_exec, os.O_WRONLY | os.O_CREAT, 0733)
-    with os.fdopen(fd, 'w') as script:
-      script.write('#!/bin/sh\n')
-      script.write('# Auto-generated wrapper script\n')
-      script.write('thisdir="$(dirname "$0")"\n')
-      script.write('LD_LIBRARY_PATH=\n')
-      script.write('exec "$thisdir/%s/ld-linux-x86-64.so.2"'
-                   ' --library-path "$thisdir/%s"'
-                   ' "$thisdir/%s.bin" "$@"\n' %
-                   (LIB_DIR, LIB_DIR, base_exec))
+  for source_dir, target_dir in shell_libraries.iteritems():
+    src = os.path.expanduser(source_dir)
+    dest = os.path.join(zip_base, target_dir)
+    logging.debug('Copying %s -> %s', src, dest)
+    shutil.copytree(src, dest)
 
 
-def CleanUp(temp_dir):
-  """Cleans up the tempdir
-    Args:
-      temp_dir = name of the directory to cleanup
+def GenerateZipFile(zip_file, zip_base):
+  """Create the specified zip file with contents of a directory.
+
+  Args:
+    zip_file: name of the zip file.
+    zip_base: The directory that we should zip.
   """
-  if os.path.exists(temp_dir):
-    shutil.rmtree(temp_dir, ignore_errors=True)
-    logging.debug('Removed tempdir = %s', temp_dir)
+  # Make sure the directory for the output file exists.
+  zip_output_dir = os.path.dirname(zip_file)
+  if not os.path.exists(zip_output_dir):
+    os.makedirs(zip_output_dir)
 
+  # Make sure the output zip file doesn't exist, so we start clean.
+  if os.path.exists(zip_file):
+    os.unlink(zip_file)
 
-def GenerateZipFile(base_name, root_dir):
-  """Returns true if able to generate zip file
-    Args:
-      base_name: name of the zip file
-      root_dir: location of the directory that we should zip
-    Returns:
-      True if successfully generates the zip file otherwise False
-  """
-  logging.debug('Generating zip file %s with contents from %s', base_name,
-               root_dir)
+  logging.debug('Generating zip file %s with contents from %s', zip_file,
+               zip_base)
   current_dir = os.getcwd()
-  os.chdir(root_dir)
   try:
-    subprocess.Popen(['zip', '-r', '-9', base_name, '.'],
-                     stdout=subprocess.PIPE).communicate()[0]
-  except OSError, e:
-   logging.error('Execution failed:%s', e.strerror)
-   return False
+    os.chdir(zip_base)
+    subprocess.check_call(['zip', '-r', '-9', zip_file, '.'])
   finally:
     os.chdir(current_dir)
-
-  return True
-
-
-def _ExcludeBlacklist(library_list, black_list=[]):
-  """Deletes the set of files from black_list from the library_list
-    Args:
-      library_list: List of the library names to filter through black_list
-      black_list: List of the black listed names to filter
-    Returns:
-      Filtered library_list
-  """
-
-  if not black_list:
-    return library_list
-
-  return_list = []
-  pattern = re.compile(r'|'.join(black_list))
-
-  logging.debug('PATTERN: %s=', pattern)
-
-  for library in library_list:
-    if pattern.search(library):
-      logging.debug('BLACK-LISTED = %s=', library)
-      continue
-    return_list.append(library)
-
-  logging.debug('Returning return_list=%s=', return_list)
-
-  return return_list
-
-
-def _EnforceWhiteList(library_list, white_list=[]):
-  """Deletes the set of files from black_list from the library_list
-    Args:
-      library_list: List of the library names to filter through black_list
-      black_list: List of the black listed names to filter
-    Returns:
-      Filtered library_list
-  """
-
-  for white_item in white_list:
-    pattern = re.compile(white_item)
-
-    logging.debug('PATTERN: %s=', pattern)
-
-    found = False
-    for library in library_list:
-      if pattern.search(library):
-        found = True
-        break
-
-    if not found:
-      logging.error('Required WHITE_LIST items %s not found!!!' % white_item)
-      exit(1)
-
-
-def CopyZipToFinalDestination(output_dir, zip_file_name):
-  """Copies the generated zip file to a final destination
-  Args:
-    output_dir: Directory where the file should be copied to
-    zip_file_name: name of the zip file that should be copied
-  Returns:
-    True on Success False on Failure
-  """
-  if not os.path.isfile(zip_file_name):
-    logging.error("Zip file %s doesn't exist. Returning False", zip_file_name)
-    return False
-
-  if not os.path.isdir(output_dir):
-    logging.debug('Creating %s', output_dir)
-    os.makedirs(output_dir)
-  logging.debug('Copying %s to %s', zip_file_name, output_dir)
-  shutil.copy2(zip_file_name, output_dir)
-  return True
 
 
 def main():
   """Main function to start the script"""
   parser = optparse.OptionParser()
 
-  parser.add_option( '-d', '--debug', dest='debug', action='store_true',
-                     default=False, help='Verbose Default: False',)
-  parser.add_option('-o', '--output-dir', dest='output_dir',
-                    default='/tmp/au-generator',
-                    help='Specify the output location for copying the zipfile')
-  parser.add_option('-z', '--zip-name', dest='zip_name',
-                    default='au-generator.zip', help='Name of the zip file')
-  parser.add_option('-k', '--keep-temp', dest='keep_temp', default=False,
-                    action='store_true', help='Keep the temp files...',)
+  parser.add_option(
+      '-d', '--debug', dest='debug', action='store_true',
+      default=False, help='Verbose [%default]',)
+  parser.add_option(
+      '-o', '--output-dir', dest='output_dir',
+      default='/tmp/au-generator',
+      help='The output location for copying the zipfile [%default]')
+  parser.add_option(
+      '-z', '--zip-name', dest='zip_name',
+      default='au-generator.zip', help='Name of the zip file. [%default]')
+  parser.add_option(
+      '-k', '--keep-temp', dest='keep_temp', default=False,
+      action='store_true', help='Keep the temp files... [%default]',)
 
-  (options, args) = parser.parse_args()
+  logging_format = '%(asctime)s - %(filename)s - %(levelname)-8s: %(message)s'
+  date_format = '%Y/%m/%d %H:%M:%S'
+  logging.basicConfig(level=logging.INFO, format=logging_format,
+                      datefmt=date_format)
+
+  (options, _) = parser.parse_args()
   if options.debug:
     logging.getLogger().setLevel(logging.DEBUG)
 
   logging.debug('Options are %s ', options)
 
-  temp_dir = CreateTempDir()
-  dest_files_root = os.path.join(temp_dir, 'au-generator')
-  os.makedirs(dest_files_root)
-  CopyRequiredFiles(dest_files_root=dest_files_root)
-  WrapExecutableFiles(dest_files_root=dest_files_root)
-  zip_file_name = os.path.join(temp_dir, options.zip_name)
-  GenerateZipFile(zip_file_name, dest_files_root)
-  CopyZipToFinalDestination(options.output_dir, zip_file_name)
-  logging.info('Generated %s/%s' % (options.output_dir, options.zip_name))
+  zip_base = None
+  try:
+    zip_base = tempfile.mkdtemp(suffix='au', prefix='tmp')
+    logging.debug('Using tempdir = %s', zip_base)
 
-  if not options.keep_temp:
-    CleanUp(temp_dir)
+    CopyExecutableFiles(EXECUTABLE_FILES, zip_base)
+    CopyShellLibraries(SHELL_LIBRARIES, zip_base)
+
+    zip_file = os.path.join(options.output_dir, options.zip_name)
+    GenerateZipFile(zip_file, zip_base)
+    logging.info('Generated %s' % zip_file)
+
+  finally:
+    if zip_base and not options.keep_temp:
+      shutil.rmtree(zip_base, ignore_errors=True)
+      logging.debug('Removed tempdir = %s', zip_base)
 
 if __name__ == '__main__':
   main()
