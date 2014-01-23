9af5231c241884b50cabfe6bf93aebf66275b646
Christopher Wiley <wiley@chromium.org>
2014-01-23 14:55:46 -0800

    Add debugging for failures to umount partitions
    BUG=chromium:337490
    TEST=None
    
    Change-Id: I3d0db3cb3a10761eeca50378b3f6ea9a9d109b81
    Reviewed-on: https://chromium-review.googlesource.com/183719
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Tested-by: Christopher Wiley <wiley@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Christopher Wiley <wiley@chromium.org>

--- build_library/disk_layout_util.sh
+++ build_library/disk_layout_util.sh
@@ -375,6 +375,8 @@ mk_fs() {
   fi
   sudo_multi "${cmds[@]}"
 
+  # In case the umount fails, print processes using the mount point.
+  fuser -vm "${mount_dir}"
   # Deletes associated loopback device as well.
   sudo umount -d "${mount_dir}"
   rm -rf "${mount_dir}"
