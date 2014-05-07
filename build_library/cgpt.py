4a3649ffa04a94953118917a20908701d22e5c0b
Gwendal Grignou <gwendal@chromium.org>
2014-05-07 14:23:17 -0700

    Introduce common layout as parent of all layouts
    Introduce 'common' layout. By default 'base' - the layout installed on
    the fixed device - is identical to 'common'.
    However, on some installs, we now can make changes in 'base' that we
    do not want to percolate into the usb/factory devices, leaving 'common'
    clean.
    
    BUG=chromium:370706
    TEST=Check tails. Verify falco layout is not changed.
    TEST=(output of cgpt.py write)
    TEST=Install falco with cros flash.
    CQ-DEPEND=CL:*163775,CL:*163759,CL:200161
    
    Change-Id: I24bea879c3c7c3fb303c4dc882b456f1b7053e7b
    Reviewed-on: https://chromium-review.googlesource.com/198724
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

--- build_library/cgpt.py
+++ build_library/cgpt.py
@@ -33,6 +33,7 @@ class InvalidSize(Exception):
 class ConflictingOptions(Exception):
   """Conflicting Options"""
 
+COMMON_LAYOUT = 'common'
 BASE_LAYOUT = 'base'
 
 
@@ -137,14 +138,14 @@ def _LoadStackedPartitionConfig(filename):
     config = json.load(f)
 
   # Let's first apply our new configs onto base.
-  base_layout = config['layouts'].setdefault(BASE_LAYOUT, [])
+  common_layout = config['layouts'].setdefault(COMMON_LAYOUT, [])
   for layout_name, layout in config['layouts'].iteritems():
     # Don't apply on yourself.
-    if layout_name == BASE_LAYOUT or layout_name == '_comment':
+    if layout_name == COMMON_LAYOUT or layout_name == '_comment':
       continue
 
     # Need to copy a list of dicts so make a deep copy.
-    working_layout = copy.deepcopy(base_layout)
+    working_layout = copy.deepcopy(common_layout)
     _ApplyLayoutOverrides(working_layout, layout)
     config['layouts'][layout_name] = working_layout
 
@@ -181,19 +182,19 @@ def _LoadStackedPartitionConfig(filename):
     new_layouts = config_layouts - parent_layouts
 
     # Actually add the copy. Use a copy such that each is unique.
-    parent_base_layout = parent_config['layouts'].setdefault(BASE_LAYOUT, [])
+    parent_cmn_layout = parent_config['layouts'].setdefault(COMMON_LAYOUT, [])
     for layout_name in new_layouts:
-      parent_config['layouts'][layout_name] = copy.deepcopy(parent_base_layout)
+      parent_config['layouts'][layout_name] = copy.deepcopy(parent_cmn_layout)
 
     # Iterate through each layout in the parent config and apply the new layout.
-    base_layout = config['layouts'].setdefault(BASE_LAYOUT, [])
+    common_layout = config['layouts'].setdefault(COMMON_LAYOUT, [])
     for layout_name, parent_layout in parent_config['layouts'].iteritems():
       if layout_name == '_comment':
         continue
 
       layout_override = config['layouts'].setdefault(layout_name, [])
-      if layout_name != BASE_LAYOUT:
-        _ApplyLayoutOverrides(parent_layout, base_layout)
+      if layout_name != COMMON_LAYOUT:
+        _ApplyLayoutOverrides(parent_layout, common_layout)
 
       _ApplyLayoutOverrides(parent_layout, layout_override)
 
