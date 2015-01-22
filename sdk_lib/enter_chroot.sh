108bbf229b9c7321bb8086cce424e56baae8ed2e
David James <davidjames@google.com>
2015-01-21 20:16:46 -0800

    Copy .gitcookies into the chroot.
    See b/19100781
    
    BUG=chromium:450890
    TEST=Test that git remote update works in the chroot now without a
         .netrc file
    
    Change-Id: Ieb38f5c42b989b3a374696991569907f2900872b
    Reviewed-on: https://chromium-review.googlesource.com/242453
    Reviewed-by: Gabe Black <gabeblack@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: David James <davidjames@chromium.org>
    Commit-Queue: David James <davidjames@chromium.org>

--- sdk_lib/enter_chroot.sh
+++ sdk_lib/enter_chroot.sh
@@ -83,6 +83,7 @@ FILES_TO_COPY_TO_CHROOT=(
   .gdata_token                # Auth token for Google Docs on chromium.org
   .disable_build_stats_upload # Presence of file disables command stats upload
   .netrc                      # May contain required source fetching credentials
+  .gitcookies                 # May contain required source fetching credentials
 )
 
 INNER_CHROME_ROOT=$FLAGS_chrome_root_mount  # inside chroot
