870f14a053c0b31ca815ef896549defeeb07fc1f
Bertrand SIMONNET <bsimonnet@chromium.org>
2015-06-01 09:49:58 -0700

    board_options: Don't infer ARCH from CHOST.
    ARCH was added to make.conf.board_setup so we no longer need to guess it
    from CHOST.
    
    BUG=None
    TEST=Trybot run on paladin for all architectures.
    
    CQ-DEPEND=CL:274433
    
    Change-Id: I8d76d81f50e794cf856d7495f907de59d023067e
    Reviewed-on: https://chromium-review.googlesource.com/274238
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- build_library/board_options.sh
+++ build_library/board_options.sh
@@ -16,24 +16,3 @@ fi
 
 # What cross-build are we targeting?
 . "${BOARD_ROOT}/etc/make.conf.board_setup"
-
-# Figure out ARCH from the given toolchain.
-# TODO(jrbarnette): There's a copy of this code in setup_board;
-# it should be shared.
-case "$(echo "${CHOST}" | awk -F'-' '{ print $1 }')" in
-  arm*)
-    ARCH="arm"
-    ;;
-  *86)
-    ARCH="x86"
-    ;;
-  *x86_64)
-    ARCH="amd64"
-    ;;
-  mips*)
-    ARCH="mips"
-    ;;
-  *)
-    error "Unable to determine ARCH from toolchain: ${CHOST}"
-    exit 1
-esac
