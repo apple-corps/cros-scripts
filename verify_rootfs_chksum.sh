3e05772ccb0a875c01fd4da5fd2742855645874b
Steven 'Steve' Kendall <skendall@neverware.com>
2016-06-08 13:33:12 -0400

    Use constants for partition numbers in verify_rootfs_chksum.sh
    BUG=None
    TEST=built and booted an image, ensured mounts were recognized
    
    Change-Id: Id4a13d7f2993a14ea643da7ed9057eb4c5a37bf5
    Reviewed-on: https://chromium-review.googlesource.com/350840
    Commit-Ready: Steven 'Steve' Kendall <skendall@neverware.com>
    Tested-by: Steven 'Steve' Kendall <skendall@neverware.com>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- verify_rootfs_chksum.sh
+++ verify_rootfs_chksum.sh
@@ -8,6 +8,8 @@
 
 SCRIPT_ROOT=$(dirname $(readlink -f "$0"))
 . "${SCRIPT_ROOT}/common.sh" || exit 1
+# for partition constants
+. "${BUILD_LIBRARY_DIR}/disk_layout_util.sh" || exit 1
 
 # Script must be run inside the chroot.
 restart_in_chroot_if_needed "$@"
@@ -39,17 +41,17 @@ switch_to_strict_mode
 
 get_partitions() {
   if [ -b ${FLAGS_image} ] ; then
-    KERNEL_IMG=$(make_partition_dev "${FLAGS_image}" 2)
-    ROOTFS_IMG=$(make_partition_dev "${FLAGS_image}" 3)
+    KERNEL_IMG=$(make_partition_dev "${FLAGS_image}" ${PARTITION_NUM_KERN_A})
+    ROOTFS_IMG=$(make_partition_dev "${FLAGS_image}" ${PARTITION_NUM_ROOT_A})
     return
   fi
 
   KERNEL_IMG=$(mktemp)
   ROOTFS_IMG=$(mktemp)
-  local kernel_offset=$(partoffset "${FLAGS_image}" 2)
-  local kernel_count=$(partsize "${FLAGS_image}" 2)
-  local rootfs_offset=$(partoffset "${FLAGS_image}" 3)
-  local rootfs_count=$(partsize "${FLAGS_image}" 3)
+  local kernel_offset=$(partoffset "${FLAGS_image}" ${PARTITION_NUM_KERN_A})
+  local kernel_count=$(partsize "${FLAGS_image}" ${PARTITION_NUM_KERN_A})
+  local rootfs_offset=$(partoffset "${FLAGS_image}" ${PARTITION_NUM_ROOT_A})
+  local rootfs_count=$(partsize "${FLAGS_image}" ${PARTITION_NUM_ROOT_A})
 
   # TODO(tgao): use loop device to save 1GB in temp space
   dd if="${FLAGS_image}" of=${KERNEL_IMG} bs=512 skip=${kernel_offset} \
