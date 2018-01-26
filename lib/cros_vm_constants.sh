6c7947c9ec9ad8958234b11a2c8a2760deca2153
Nicolas Norvez <norvez@chromium.org>
2018-01-26 10:25:09 -0800

    cros_vm_constants: remove obsolete constants
    No CS hits for VMDK, VMX, VBOX constants. Last use of DEFAULT_MEM has
    also just been removed now that image_to_vm.sh recommends using cros_vm.
    
    BUG=chromium:782664
    TEST=Looked up CS, Pre-CQ and CQ should confirm.
    
    Change-Id: Iaedd835c99db0033b6b72e606aa1225531fdd353
    Reviewed-on: https://chromium-review.googlesource.com/889409
    Commit-Ready: Nicolas Norvez <norvez@chromium.org>
    Tested-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>

--- lib/cros_vm_constants.sh
+++ lib/cros_vm_constants.sh
@@ -5,8 +5,4 @@
 # Common constants for vm scripts.
 
 # Default values for creating VM's.
-DEFAULT_MEM="1024"
-DEFAULT_VMDK="ide.vmdk"
-DEFAULT_VMX="chromiumos.vmx"
-DEFAULT_VBOX_DISK="os.vdi"
 DEFAULT_QEMU_IMAGE="chromiumos_qemu_image.bin"
