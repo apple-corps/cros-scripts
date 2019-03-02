3131d4119ed6c4f63122c86f594bfee574f30c63
Mike Frysinger <vapier@chromium.org>
2019-03-02 14:09:37 -0500

    build_packages: drop remnants of command stat generation
    We stopped uploading these stats about 2 years ago (CL:450846).
    Drop the generation code as nothing reads the contents anymore.
    
    BUG=chromium:698105
    TEST=precq passes
    
    Change-Id: Ib839773fb9d83c90f5273bf195ce9f9e5d7d4f5e
    Reviewed-on: https://chromium-review.googlesource.com/1498112
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Alex Klein <saklein@chromium.org>

--- common.sh
+++ common.sh
@@ -811,10 +811,6 @@ bd_safe_size() {
   fi
 }
 
-get_git_id() {
-  git var GIT_COMMITTER_IDENT | sed -e 's/^.*<\(\S\+\)>.*$/\1/'
-}
-
 # Fixes symlinks that are incorrectly prefixed with the build root $1
 # rather than the real running root '/'.
 # TODO(sosa) - Merge setup - cleanup below with this method.
@@ -987,51 +983,11 @@ print_time_elapsed() {
   fi
 }
 
-# Associative array for filling in extra command-specific stats before
-# calling command_completed.
-declare -A EXTRA_COMMAND_STATS
-
-# Save original command line.
-command_line_arr=( "$0" "$@" )
-
 command_completed() {
   # Call print_elapsed_time regardless.
   local run_time=$(get_elapsed_seconds)
-  local cmd_base=$(basename "${command_line_arr[0]}")
+  local cmd_base=$(basename "$0")
   print_time_elapsed ${run_time} ${cmd_base}
-
-  # Prepare command stats in an associative array.  Additional command-specific
-  # stats can be added through EXTRA_COMMAND_STATS associative array.
-  declare -A stats
-  stats=(
-    [cmd_line]=${command_line_arr[*]}
-    [cmd_base]=${cmd_base}
-    [cmd_args]=${command_line_arr[*]:1}
-    [run_time]=${run_time}
-    [username]=$(get_git_id)
-    [board]=${FLAGS_board}
-    [host]=$(hostname -f)
-    [cpu_count]=$(grep -c processor /proc/cpuinfo)
-    [cpu_type]=$(uname -p)
-  )
-
-  local attr
-  for attr in "${!EXTRA_COMMAND_STATS[@]}"; do
-    stats[${attr}]=${EXTRA_COMMAND_STATS[${attr}]}
-  done
-
-  # Prepare temporary file for stats.
-  local tmpfile=$(mktemp -t tmp.stats.XXXXXX)
-  trap "rm -f '${tmpfile}'" EXIT
-
-  # Write stats out to temporary file.
-  echo "Chromium OS Build Command Stats - Version 1" > "${tmpfile}"
-  for attr in "${!stats[@]}"; do
-    echo "${attr} ${stats[${attr}]}"
-  done >> "${tmpfile}"
-
-  rm "${tmpfile}"
-  trap - EXIT
 }
 
 # The board and variant command line options can be used in a number of ways
