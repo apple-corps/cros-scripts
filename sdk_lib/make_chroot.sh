93cce81fb80fcc32bf30b2cb866c6e230a54bff7
Mike Frysinger <vapier@chromium.org>
2021-03-25 20:11:36 -0400

    sdk: move chromiumos-niceties.sh to chromite
    This makes it easier to manage & install updates.
    
    BUG=None
    TEST=CQ passes
    
    Change-Id: I15cce1cd32c539dc1def45c241d97fc55ec172aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosutils/+/2787569
    Reviewed-by: Sergey Frolov <sfrolov@google.com>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- sdk_lib/make_chroot.sh
+++ sdk_lib/make_chroot.sh
@@ -304,14 +304,9 @@ EOF
 
    target="${FLAGS_chroot}/etc/profile.d"
    mkdir -p "${target}"
-   cat << EOF > "${target}/chromiumos-niceties.sh"
-# Niceties for interactive logins. (cr) denotes this is a chroot, the
-# __git_branch_ps1 prints current git branch in ./ . The $r behavior is to
-# make sure we don't reset the previous $? value which later formats in
-# $PS1 might rely on.
-PS1='\$(r=\$?; __git_branch_ps1 "(%s) "; exit \$r)'"\${PS1}"
-PS1="(cr) \${PS1}"
-EOF
+   ln -sfT \
+     "/mnt/host/source/chromite/sdk/etc/profile.d/50-chromiumos-niceties.sh" \
+     "${target}/50-chromiumos-niceties.sh"
 
    # Select a small set of locales for the user if they haven't done so
    # already.  This makes glibc upgrades cheap by only generating a small
